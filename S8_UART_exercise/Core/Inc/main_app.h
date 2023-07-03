ba73422d6347e824e83f5c4eef4d853b6a32f445
Rene Brglez <rene.brglez@gmail.com>
2023-07-03 14:37:59 +0200

    Created HAL_MspInit(), which sets up priority grouping, Enables some system exceptions (usage fault, bus fault, memory fault) and Configures the priority for system exceptions using HAL API.

--- S8_UART_exercise/Core/Inc/main_app.h
+++ S8_UART_exercise/Core/Inc/main_app.h
@@ -8,6 +8,12 @@
 #ifndef INC_MAIN_APP_H_
 #define INC_MAIN_APP_H_
 
+// Use NVIC_PRIORITYGROUP_4, which has 16 different preempt-priorities
+#define  MEM_MNG_PRIORITY	0U /*!< memory management interrupt preempt-priority */
+#define  BUS_FLT_PRIORITY	0U /*!< bus fault interrupt preempt-priority */
+#define  USG_FLT_PRIORITY	0U /*!< usage fault interrupt preempt-priority */
 
+// Redundant because NVIC_PRIORITYGROUP_4 is used, which has only one sub-priority!
+#define  DEFAULT_SUB_PRIORITY	0U /*!< Default interrupt sub-priority */
 
 #endif /* INC_MAIN_APP_H_ */
--- S8_UART_exercise/Core/Src/msp.c
+++ S8_UART_exercise/Core/Src/msp.c
@@ -5,4 +5,28 @@
  *      Author: Rene
  */
 
+#include "stm32wbxx_hal.h"
+#include "main_app.h"
 
+
+void HAL_MspInit(void)
+{
+	// Function called by HAL_Init()
+	// Initialize low-level processor specific inits
+
+	// 1.) Set up the priority grouping of the arm cortex mx processor
+	// 	NOTE: Redundant, because this is default setting(This is done in HAL_Init())
+	HAL_NVIC_SetPriorityGrouping(NVIC_PRIORITYGROUP_4);
+
+	// 2.) Enable the required system exceptions of the arm cortex mx processor
+	// 	NOTE: Enabled usage fault, memory fault and bus fault exceptions.
+	SCB -> SHCSR |= SCB_SHCSR_USGFAULTENA_Msk;
+	SCB -> SHCSR |= SCB_SHCSR_BUSFAULTENA_Msk;
+	SCB -> SHCSR |= SCB_SHCSR_MEMFAULTENA_Msk;
+
+
+	// 3.) Configure the priority for the system exceptions
+	HAL_NVIC_SetPriority(MemoryManagement_IRQn, MEM_MNG_PRIORITY, DEFAULT_SUB_PRIORITY);
+	HAL_NVIC_SetPriority(BusFault_IRQn, BUS_FLT_PRIORITY, DEFAULT_SUB_PRIORITY);
+	HAL_NVIC_SetPriority(UsageFault_IRQn, BUS_FLT_PRIORITY, DEFAULT_SUB_PRIORITY);
+}

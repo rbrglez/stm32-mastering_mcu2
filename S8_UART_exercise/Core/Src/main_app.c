8a75c2c8e0ecc316616a2f32ab5f87aa52f30c74
Rene Brglez <rene.brglez@gmail.com>
2023-07-03 14:39:15 +0200

    High level UART initialization (link handle with uart base address, set baud rate, word length, ...) Initialize UART using HAL API.

--- S8_UART_exercise/Core/Src/main_app.c
+++ S8_UART_exercise/Core/Src/main_app.c
@@ -9,18 +9,45 @@
 #include "stm32wbxx_hal.h"
 #include "main_app.h"
 
+UART_HandleTypeDef huart1;
+
 void SystemClockConfig(void);
+void UART1_Init(void);
+void Error_handler(void);
 
 int main(void)
 {
 	HAL_Init();
 	SystemClockConfig();
+	UART1_Init();
 
 	return 0;
 }
 
 
 void SystemClockConfig(void)
+{
+
+}
+
+void UART1_Init(void)
+{
+	huart1.Instance = USART1;
+
+	huart1.Init.BaudRate = 115200;
+	huart1.Init.WordLength = UART_WORDLENGTH_8B;
+	huart1.Init.StopBits = UART_STOPBITS_1;
+	huart1.Init.HwFlowCtl = UART_HWCONTROL_NONE;
+	huart1.Init.Mode = UART_MODE_TX_RX;
+
+	if (HAL_UART_Init(&huart1) != HAL_OK)
+	{
+		// There is a problem
+		Error_handler();
+	}
+}
+
+void Error_handler(void)
 {
 
 }
